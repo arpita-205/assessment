a3e2bafc04df0ee6c82406032002057a923a4d43
Arpita Gupta <arpitagupta205@gmail.com>
2025-11-11 07:20:48 +0530

    fix: added theme

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import data from '../data/data.json';
 import { AgGridReact } from 'ag-grid-react';
-import { AllCommunityModule, ModuleRegistry,themeQuartz,colorSchemeLightCold } from "ag-grid-community";
+import {
+  AllCommunityModule,
+  ModuleRegistry,
+  themeQuartz,
+  colorSchemeLightCold,
+ colorSchemeDarkBlue
+} from "ag-grid-community";
 import { getAgGridColumns } from '../utils';
 
 
@@ -9,7 +15,7 @@ ModuleRegistry.registerModules([AllCommunityModule]);
 
 const Dashboard = () => {
   const [rowData, setRowData] = useState(data.employees);
-   const themeLightCold = themeQuartz.withPart(colorSchemeLightCold);
+   const [darkMode, setDarkMode] = useState(false);
   const [colDefs, setColDefs] = useState(
     getAgGridColumns(data.employees, ["firstName", "lastName"])
   );
@@ -28,7 +34,14 @@ const Dashboard = () => {
      );
   };
 
-  
+  const theme = useMemo(() => {
+    const base = darkMode
+      ? themeQuartz.withPart(colorSchemeDarkBlue)
+      : themeQuartz.withPart(colorSchemeLightCold);
+
+    return base
+  }, [darkMode]);
+
   useEffect(() => {
     setColDefs((prev) =>
       prev.map((c) =>
@@ -52,20 +65,27 @@ const Dashboard = () => {
 
   return (
     <div className="w-full h-full px-4 py-2">
+      <div className="flex flex-col gap-4 pb-4">
         <h2 className="text-blue-600 font-bold mb-4 text-xl">
           Employee Dashboard
         </h2>
-        <div className='w-full h-full'>
-          <div  className=' h-full w-full'>
-            <AgGridReact
-              theme={themeLightCold}
-              rowData={rowData}
-              columnDefs={colDefs}
-              pagination={true}
-              paginationPageSize={20}
-              defaultColDef={defaultColDef}
-            />
-          
+        <button
+          onClick={() => setDarkMode((mode) => !mode)}
+          className="border px-3 py-1 rounded text-sm hover:bg-blue-100 dark:hover:bg-blue-900 w-fit"
+        >
+          <span className='font-medium'>Theme:</span> {darkMode ? " Dark" : "Light"}
+        </button>
+      </div>
+      <div className="w-full h-full">
+        <div className=" h-full w-full">
+          <AgGridReact
+            theme={theme}
+            rowData={rowData}
+            columnDefs={colDefs}
+            pagination={true}
+            paginationPageSize={20}
+            defaultColDef={defaultColDef}
+          />
         </div>
       </div>
     </div>
