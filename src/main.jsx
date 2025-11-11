0f75003b3cc09a226b676932f66460b5a738fe20
Arpita Gupta <arpitagupta205@gmail.com>
2025-11-11 07:02:01 +0530

    fox: deploy configuration

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <div className='w-full h-screen m-0 p-0'>
+    <BrowserRouter basename="/assessment">
+      <div className="w-full h-screen m-0 p-0">
         <App />
       </div>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
